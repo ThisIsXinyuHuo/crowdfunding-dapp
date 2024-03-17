ff80928ddaf20d1ded7d97da53064e0bec54ad42
ThisIsXinyuHuo <xinyu.huo@mail.utoronto.ca>
2024-03-17 14:08:00 -0700

    Alert and Disconnect

--- frontend/src/components/ButtonVariant.jsx
+++ frontend/src/components/ButtonVariant.jsx
@@ -1,4 +1,4 @@
-const ButtonVariant = ({type, text, clickHandler, style}) => {
+const ButtonVariant = ({type, text, clickHandler, style, disabled}) => {
     // type is the button type
     // text is the text shown in the button
     // clickHandler goes into onClick
@@ -10,6 +10,7 @@ const ButtonVariant = ({type, text, clickHandler, style}) => {
             className = {`px-6 py-2.5 bg-gray-600 hover:bg-gray-700  text-white font-semibold text-xs leading-tight rounded-full shadow-md ${style}`}
             
             onClick={clickHandler}
+            disabled = {disabled}
         >
             {text}
         </button>
