57b9fa902de5f95199e55a8380c10bfabc7ac794
ThisIsXinyuHuo <xinyu.huo@mail.utoronto.ca>
2024-04-08 09:34:24 -0700

    user profile (placeholder), deploy script, and hard config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    const balance = await ethers.provider.getBalance(taskArgs.account);
+
+    console.log(ethers.formatEther(balance), "ETH");
+  });
+
+
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+      hardhat: {
+        chainId: 31337,
+        blockConformations: 1,
+        allowUnlimitedContractSize: true
+      },
+      sepolia: {
+        chainId: 11155111,
+        url: process.env.SEPOLIA_API_URL,
+        accounts: [process.env.SEPOLIA_PRIVATE_KEY]
+      },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,11 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Contract = await hre.ethers.getContractFactory("Crowdfunding");
+  const contract = await Contract.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  await contract.waitForDeployment();
+  console.log("Crowdfunding deployed to:", await contract.getAddress());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
